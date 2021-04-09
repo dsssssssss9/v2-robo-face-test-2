0efb99a74d2010731bc0e38c21ecc9020f7a055b
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-04-09 01:28:36 +0100

    add smile and frown funtions

--- main.ts
+++ main.ts
@@ -10,12 +10,40 @@ function show_eyes () {
 input.onButtonPressed(Button.A, function () {
     show_nose()
 })
+function show_smile () {
+    for (let index = 0; index <= smile.length - 1; index++) {
+        strip.setPixelColor(smile[index], neopixel.colors(NeoPixelColors.Violet))
+        strip.show()
+    }
+    basic.pause(100)
+    strip.clear()
+    strip.show()
+}
 function init () {
     nose = 14
     lefteye = 15
     righteye = 16
     botheyes = [15, 16]
+    smile = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5
+    ]
+    frown = [
+    0,
+    5,
+    6,
+    7,
+    8,
+    9
+    ]
 }
+input.onButtonPressed(Button.AB, function () {
+    show_smile()
+})
 input.onButtonPressed(Button.B, function () {
     show_eyes()
 })
@@ -26,9 +54,11 @@ function show_nose () {
     strip.clear()
     strip.show()
 }
+let frown: number[] = []
 let righteye = 0
 let lefteye = 0
 let nose = 0
+let smile: number[] = []
 let botheyes: number[] = []
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P0, 17, NeoPixelMode.RGB)
